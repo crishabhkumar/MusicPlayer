4a0f9f4b4856c4a126df00bad07e7fc5003c319b
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-28 19:57:33 +0530

    Button working check is done.

--- app/src/main/java/com/rishabhkumar/musicplayer/MainActivity.kt
+++ app/src/main/java/com/rishabhkumar/musicplayer/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.rishabhkumar.musicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.rishabhkumar.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_MusicPlayer)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //for checking buttons are working properly or not
+        binding.btnShuffle.setOnClickListener{
+            Toast.makeText(this,"Shuffle button clicked.",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnFavourite.setOnClickListener{
+            Toast.makeText(this,"Favourite button clicked.",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnPlaylist.setOnClickListener{
+            Toast.makeText(this,"Playlist button clicked.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
