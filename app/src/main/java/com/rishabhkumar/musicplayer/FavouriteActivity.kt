58e91151fb84d2a1366bb3368c0d76a94afd9ae9
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-28 18:59:34 +0530

    UI for favourite and playlist activity and some code commenting is done.

---
+++ app/src/main/java/com/rishabhkumar/musicplayer/FavouriteActivity.kt
@@ -0,0 +1,11 @@
+package com.rishabhkumar.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavouriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite)
+    }
+}
