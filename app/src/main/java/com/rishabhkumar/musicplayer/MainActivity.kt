b3b9bfe55cad9d886d052452288451db95242088
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-28 21:33:29 +0530

    Permission for accessing the phone storage is done.

--- app/src/main/java/com/rishabhkumar/musicplayer/MainActivity.kt
+++ app/src/main/java/com/rishabhkumar/musicplayer/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.rishabhkumar.musicplayer
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.rishabhkumar.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestRunTimePermission()
         setTheme(R.style.Theme_MusicPlayer)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -32,6 +37,26 @@ class MainActivity : AppCompatActivity() {
 //            Toast.makeText(this,"Playlist button clicked.",Toast.LENGTH_SHORT).show()
             val intent = Intent(this@MainActivity,PlaylistActivity::class.java)
             startActivity(intent)
+        }
+        requestRunTimePermission()
+
+    }
+    //for requesting permission
+    private fun requestRunTimePermission(){
+        if(ActivityCompat.checkSelfPermission(this@MainActivity,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),11)
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == 11){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission granted.",Toast.LENGTH_SHORT).show()
+            else{
+                ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),11)
+            }
         }
 
     }
