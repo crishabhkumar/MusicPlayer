ca91a6880a556d5f5ab8518ea174720a66960e73
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-29 23:10:31 +0530

    Next and previous button fucntionality is done.

--- app/src/main/java/com/rishabhkumar/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/rishabhkumar/musicplayer/PlayerActivity.kt
@@ -12,7 +12,7 @@ class PlayerActivity : AppCompatActivity() {
         lateinit var musicListPlayer : ArrayList<Music>
         var songPosition: Int = 0
         var mediaPlayer: MediaPlayer ?= null
-        var isPlaying : Boolean = false
+        var isPlaying : Boolean = false //to check whether song is currently playing or not
     }
 
     private lateinit var binding : ActivityPlayerBinding
@@ -26,8 +26,16 @@ class PlayerActivity : AppCompatActivity() {
             if(isPlaying) pauseMusic()
             else playMusic()
         }
+        binding.btnPreviourPlayer.setOnClickListener{
+            prevNextSong(false)
+        }
+        binding.btnNextPlayer.setOnClickListener{
+            prevNextSong(true)
+        }
     }
 
+    //function for setting up the layout
+    //means setting up the image of sng in media player
     private fun setLayout(){
         Glide.with(this)
             .load(musicListPlayer[songPosition].artUri)
@@ -37,6 +45,7 @@ class PlayerActivity : AppCompatActivity() {
 
     }
 
+    //function for creating the media player
     private fun createMediaPlayer(){
         try{
             if(mediaPlayer == null){
@@ -52,6 +61,8 @@ class PlayerActivity : AppCompatActivity() {
             return
         }
     }
+
+    //fucntion for initializing the layout
     private fun initializeLayout(){
         songPosition = intent.getIntExtra("index",0)
         when(intent.getStringExtra("class")){
@@ -64,14 +75,48 @@ class PlayerActivity : AppCompatActivity() {
         }
     }
 
+    //function fro playing the music
     private fun playMusic(){
         binding.btnPlayAndPause.setIconResource(R.drawable.pause_icon)
         isPlaying = true
         mediaPlayer!!.start()
     }
+    //function for pausing the music
     private fun pauseMusic(){
         binding.btnPlayAndPause.setIconResource(R.drawable.play_icon)
         isPlaying = false
         mediaPlayer!!.pause()
+    }
+
+    private fun prevNextSong(increment: Boolean){
+        //if increment is true means we want to go in next song
+        //or else we want to go in previous song
+        if(increment){
+            checkSongPosition(increment = true)
+            setLayout()
+            createMediaPlayer()
+        }else{
+            checkSongPosition(increment = false)
+            setLayout()
+            createMediaPlayer()
+        }
+    }
+
+    //function for checking and setting up the song position whether it is
+    // at first or last position
+    private fun checkSongPosition(increment :Boolean){
+        if(increment){
+            if(musicListPlayer.size-1 == songPosition){
+                songPosition = 0
+            }else{
+                ++songPosition
+            }
+        }else{
+            if(songPosition == 0){
+                songPosition = musicListPlayer.size-1
+            }else{
+                --songPosition
+            }
+        }
     }
 }
