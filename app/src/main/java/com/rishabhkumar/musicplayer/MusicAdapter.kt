1006cd3f108fc5b8ae02b891febcd8cb8b0bcb71
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-29 17:53:55 +0530

    Successfully played first song.

--- app/src/main/java/com/rishabhkumar/musicplayer/MusicAdapter.kt
+++ app/src/main/java/com/rishabhkumar/musicplayer/MusicAdapter.kt
@@ -36,6 +36,8 @@ class MusicAdapter(private val context : Context,  private val musicList:ArrayLi
 
         holder.root.setOnClickListener {
             val intent = Intent(context, PlayerActivity::class.java)
+            intent.putExtra("index",position)
+            intent.putExtra("class","MusicAdapter")
             ContextCompat.startActivity(context,intent,null)
 
         }
--- app/src/main/java/com/rishabhkumar/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/rishabhkumar/musicplayer/PlayerActivity.kt
@@ -1,15 +1,37 @@
 package com.rishabhkumar.musicplayer
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rishabhkumar.musicplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+    companion object {
+        lateinit var musicListPlayer : ArrayList<Music>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer ?= null
+
+    }
+
     private lateinit var binding : ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.customPink)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        songPosition = intent.getIntExtra("index",0)
+        when(intent.getStringExtra("class")){
+            "MusicAdapter"->{
+                musicListPlayer = ArrayList()
+                musicListPlayer.addAll(MainActivity.musicListMA)
+                if(mediaPlayer == null){
+                    mediaPlayer = MediaPlayer()
+                }
+                mediaPlayer!!.reset()
+                mediaPlayer!!.setDataSource(musicListPlayer[songPosition].path)
+                mediaPlayer!!.prepare()
+                mediaPlayer!!.start()
+            }
+        }
     }
 }
