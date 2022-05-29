eaa41385752afe24acd01fc38cf9c6bed8bed037
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-29 17:34:31 +0530

    Time duration correction and intent initialization of song click to Playeractivity.

--- app/src/main/java/com/rishabhkumar/musicplayer/MusicAdapter.kt
+++ app/src/main/java/com/rishabhkumar/musicplayer/MusicAdapter.kt
@@ -1,8 +1,10 @@
 package com.rishabhkumar.musicplayer
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -14,6 +16,7 @@ class MusicAdapter(private val context : Context,  private val musicList:ArrayLi
         val album = binding.txtSongAlbum
         val image = binding.imgMusicView
         val duration = binding.txtSongDuration
+        val root = binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MyHolder {
@@ -24,11 +27,18 @@ class MusicAdapter(private val context : Context,  private val musicList:ArrayLi
     override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
-        holder.duration.text = musicList[position].duration.toString()
+//        holder.duration.text = musicList[position].duration.toString()
+        holder.duration.text = formatDuration(musicList[position].duration)
         Glide.with(context)
             .load(musicList[position].artUri)
             .apply(RequestOptions().placeholder(R.drawable.splash_screen).centerCrop())
             .into(holder.image)
+
+        holder.root.setOnClickListener {
+            val intent = Intent(context, PlayerActivity::class.java)
+            ContextCompat.startActivity(context,intent,null)
+
+        }
     }
 
     override fun getItemCount(): Int {
